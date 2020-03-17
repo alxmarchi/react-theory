3060d98fec27dceb49a9facc58e6564d06e92c3f
alxmarchi <zyex@mail.ru>
2020-03-17 23:29:41 +0300

    content and parameters transfer

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-export default () => (
+export default props => (
   <div>
-    <p>This is car component</p>
-    <p>Number: <strong>{Math.round(Math.random() * 100)}</strong></p>
+    <h3>Сar name: {props.name}</h3>
+    <p>Year: <strong>{props.year}</strong></p>
+    { props.children }
   </div>
 )
