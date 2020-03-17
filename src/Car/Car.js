f7a2a5365311ea639a5303a20bd6deec38ad4d81
alxmarchi <zyex@mail.ru>
2020-03-17 23:25:34 +0300

    simple component

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,15 +1,27 @@
 import React from 'react'
 
-export default props => (
-  <div style={{
-    border: '1px solid #ccc',
-    marginBottom: '10px',
-    display: 'block',
-    padding: '10px'
-  }}>
-    <h3>Сar name: {props.name}</h3>
-    <p>Year: <strong>{props.year}</strong></p>
-    <input type="text" onChange={props.onChangeName} value={props.name} />
-    <button onClick={props.onDelete}>Delete</button>
+// function car() {
+//   return (
+//     <div>This is car component</div>
+//   )
+// }
+
+// const car = () => {
+//   return (
+//     <div>This is car component</div>
+//   )
+// }
+
+// const car = () => (
+//   <div>
+//     This is car component
+//     <strong>test</strong>
+//   </div>
+// )
+
+export default () => (
+  <div>
+    This is car component
+    <strong>test</strong>
   </div>
 )
